438d75e1e3821a4325b4b3a89df265239352acb7
Макс Левченко <MaxiLeva@gmail.com>
2022-10-25 07:25:51 +0300

    Добавьте файлы проекта.

---
+++ 12_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Mod12_1;
+
+namespace Mod12_1
+{
+    public class Program
+    {
+        static void Main(string [] args)
+        {
+            List<User> Users = new List<User> ();
+            Users.Add(new User("log1", "name 1", true));
+            Users.Add(new User("log2", "name 2", false));
+            Users.Add(new User("log3", "name 3", true));
+            Users.Add(new User("log4", "name 4", false));
+
+            foreach (var user in Users)
+            {
+                if (user._IsPremium != true)
+                {
+                    user.ShowAds();
+                }
+                Console.WriteLine($"{user._Name}, Добрый день");
+            }
+        }
+        
+    }
+}
---
+++ 12_1/User.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Mod12_1
+{
+    public class User
+    {
+        public string _Login { get; set; }
+        public string _Name { get; set; }
+        public bool _IsPremium { get; set; }
+
+        public User (string login, string name, bool isPremium)
+        {
+            _Login = login;
+            _Name = name;
+            _IsPremium = isPremium;
+        }
+        public void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными тграми");
+            Thread.Sleep(1000);
+            Console.WriteLine("Купите подписку");
+            Thread.Sleep(2000);
+            Console.WriteLine("Оформите премиум- подписку");
+            Thread.Sleep(2000);
+        }
+    }
+
+}
